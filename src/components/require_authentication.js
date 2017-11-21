b4aa0652efcd00f152bf0efa70262e31e163135c
MrRabbitRabbit <mrrabitrabit@gmail.com>
2017-11-21 15:05:59 +0800

    41 HOC Scaffold Code
    authentication HOC  usage   pseudo code

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 export default function(ComposedComponent) {
     class Authentication extends Component {
         render(){
+            // console.log( this.props.resources);
             return <ComposedComponent {...this.props} />;
         }
     }
     return Authentication;
 }
+
+
+// // In some other location ... not in this file ..
+// // We want to use this HOC
+// import Authentication  // this is my HOC
+// import Resources // This is the component I want to wrap
+
+// const ComposedComponent = Authentication(Resources);
+
+// //In some render method...
+// <ComposedComponent resources={resourceList} />
